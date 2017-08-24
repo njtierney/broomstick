299f9dc3db144064058a9973f3e83120749f3871
Nicholas Tierney <nicholas.tierney@gmail.com>
2017-08-24 18:10:20 +1000

    first commit :tada:

---
+++ R/tidy-rpart.R
@@ -0,0 +1,46 @@
+#' tidy up the model summary of rpart
+#'
+#' tidy returns a tibble of variable importance for the rpart pacakge
+#'
+#' @param x An `rpart` model
+#' @param ... extra functions or arguments
+#'
+#' @return A tibble containing the importance score for each variable
+#'
+#' @examples
+#'
+#' # retrieve a tibble of the variable importance from an rpart model
+#'
+#' library(rpart)
+#' fit_rpart <- rpart(Kyphosis ~ Age + Number + Start, data = kyphosis)
+#'
+#' tidy(fit_rpart)
+#'
+#' @export
+tidy.rpart <- function(x, ...){
+
+  # Some trees are stumps, we need to skip those that are NULL (stumps)
+  # so here we say, "If variable importance is NOT NULL, do the following"
+  # Another option would be to only include those models which are not null.
+
+  if (is.null(x$variable.importance) == FALSE) {
+
+    x$variable.importance %>%
+      data.frame(variable = names(x$variable.importance),
+                 importance = as.vector(x$variable.importance),
+                 row.names = NULL) %>%
+      dplyr::select(variable,
+                    importance) %>%
+      tibble::as_tibble()
+
+    # if rpart_frame just contains a decision stump, make NULL datasets.
+  } else {
+
+    data.frame(variable = NULL,
+               importance = NULL,
+               row.names = NULL) %>%
+      tibble::as_tibble()
+
+  } # end else
+
+}
